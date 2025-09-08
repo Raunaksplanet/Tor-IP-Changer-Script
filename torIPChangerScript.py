aec90f1b0233a3236228ee9904a196205e01048e
Abraham Gonzalez <gonzalezabraham1@adt.com>
2025-09-08 09:44:09 -0700

    readded print

--- torIPChangerScript.py
+++ torIPChangerScript.py
@@ -51,6 +51,8 @@ CookieAuthentication 0
         subprocess.run(["brew", "services", "restart", "tor"])
     time.sleep(3)
 
+    print("[+] Tor configured and (re)started.")
+
 def is_port_in_use(port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         return s.connect_ex(("127.0.0.1", port)) == 0
